ad494acaef900f1eb20aac04085980186787d7ff
Philipp Wallrich <philipp.wallrich@inovex.de>
2022-12-04 12:17:46 +0100

    implement Day3 Part 2

--- day3/src/main.rs
+++ day3/src/main.rs
@@ -1,4 +1,5 @@
 use std::collections::{HashMap, HashSet};
+use itertools::Itertools;
 
 fn main() {
     println!("Running Day 2");
@@ -13,7 +14,7 @@ fn main() {
 // CrZsJsPPZsGzwwsLwLmpwMDw"#;
 
     run_part1(input);
-    // run_part2(input);
+    run_part2(input);
 }
 
 fn run_part1(input: &str) {
@@ -37,6 +38,46 @@ fn run_part1(input: &str) {
         });
 
     println!("part1: {:?}", result);
+}
+
+fn run_part2(input: &str) {
+    let result = input
+    .split("\n")
+    .tuples::<(_,_,_)>()
+    .map(|rucksack| -> (HashSet<char>, HashSet<char>, HashSet<char>) {
+        (HashSet::from_iter(rucksack.0.chars()), HashSet::from_iter(rucksack.1.chars()), HashSet::from_iter(rucksack.2.chars()) )
+    })
+    .map(|rucksack| {
+        let first = rucksack.0.intersection(&rucksack.1).copied();
+        return rucksack.2.intersection(&HashSet::from_iter(first)).copied().next();
+    })
+    .flatten()
+    .map(|badge| {
+        get_priority(badge)
+    })
+    .fold(0, |old, new| {
+        old + new
+    });
+    
+    // .collect::<Vec<&[&str]>>();
+        // .map(|rucksack| {
+        //      return rucksack.split_at(rucksack.len() / 2)
+        // })
+        // .map(|rucksack| -> (HashSet<char>, HashSet<char>) {
+        //     (HashSet::from_iter(rucksack.0.chars()), HashSet::from_iter(rucksack.1.chars()))
+        // })
+        // .map(|rucksack| {
+        //     rucksack.0.intersection(&rucksack.1).copied().next()
+        // })
+        // .flatten()
+        // .map(|duplicate| {
+        //     get_priority(duplicate)
+        // })
+        // .fold(0, |old, new| {
+        //     old + new
+        // });
+
+    println!("part2: {:?}", result);
 }
 
 fn get_priority(item: char) -> u32 {
